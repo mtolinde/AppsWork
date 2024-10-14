0fd1f39c689a1b876687d7398e6e57e4d3a3a280
mtolinde <71803200+mtolinde@users.noreply.github.com>
2024-10-13 23:42:32 -0700

    Add files via upload

--- Reachout-Suggestions/src/app/page.tsx
+++ Reachout-Suggestions/src/app/page.tsx
@@ -2,7 +2,7 @@ import ExcelProcessor from '@/components/excel-processor'
 import '@/app/globals.css'
 export default function Home() {
   return (
-    <main className="min-h-screen p-24">
+    <main className="min-h-screen bg-background">
       <ExcelProcessor />
     </main>
   )
